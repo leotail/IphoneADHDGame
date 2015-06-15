bd54b01fd8f242b9a967f839dafa45a127dd8db4
leotail <leotail@users.noreply.github.com>
2015-06-14 20:02:40 -0700

    Create ViewControllerViewController.h

---
+++ ViewControllerViewController.h
@@ -0,0 +1,24 @@
+//
+//  ViewControllerViewController.h
+//  MemoryGame
+//
+//  Created by Hao Zhuang on 11/23/12.
+//  Copyright (c) 2012 Hao Zhuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewControllerViewController : UIViewController
+{
+    NSMutableArray *Array1;
+    NSMutableArray *Array2;
+    NSMutableArray *Array3;
+    NSMutableArray *Array4;
+    NSMutableArray *Array5;
+    NSMutableArray *Array6;
+    NSMutableArray *Array7;
+    NSMutableArray *Array8;
+}
+
+
+@end
