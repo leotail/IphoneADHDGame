b2c6143ac91d8f77d0c17e6bd5cf8aa7bc1f28e2
leotail <leotail@users.noreply.github.com>
2015-06-14 20:06:43 -0700

    Create Selectgamestat.h

---
+++ Selectgamestat.h
@@ -0,0 +1,16 @@
+//
+//  Selectgamestat.h
+//  MemoryGame
+//
+//  Created by Hao Zhuang on 12-11-25.
+//  Copyright (c) 2012 Hao Zhuang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface Selectgamestat : ViewController
+- (IBAction)SimonScore:(id)sender;
+- (IBAction)MemoryScore:(id)sender;
+- (IBAction)MatchngScore:(id)sender;
+
+@end
