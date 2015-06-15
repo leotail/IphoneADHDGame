7a7ddbe4f9775932c3bfcef69dd6989897c4134e
leotail <leotail@users.noreply.github.com>
2015-06-14 20:06:02 -0700

    Create scorelist.h

---
+++ scorelist.h
@@ -0,0 +1,15 @@
+//
+//  scorelist.h
+//  MemoryGame
+//
+//  Created by Hao Zhuang on 12-11-25.
+//  Copyright (c) 2012 Hao Zhuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface scorelist : UITableViewController
+{
+    NSMutableArray* playerstat;
+}
+@end
