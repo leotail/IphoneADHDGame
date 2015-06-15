75a57efa7f5e5561eee76c101d029671f899b0f9
leotail <leotail@users.noreply.github.com>
2015-06-14 20:05:29 -0700

    Create list.h

---
+++ list.h
@@ -0,0 +1,15 @@
+//
+//  list.h
+//  MemoryGame
+//
+//  Created by Hao Zhuang on 12-11-25.
+//  Copyright (c) 2012 Hao Zhuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface list : UITableViewController
+{
+    NSMutableArray*myArray2;
+}
+@end
