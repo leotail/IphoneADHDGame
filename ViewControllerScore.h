8f316ebf5935f5879260a811be2cd676b39170a4
leotail <leotail@users.noreply.github.com>
2015-06-14 19:40:03 -0700

    Create ViewControllerScore.h

---
+++ ViewControllerScore.h
@@ -0,0 +1,16 @@
+//
+//  ViewControllerScore.h
+//  MemoryGame
+//
+//  Created by Hao Zhuang on 11/11/12.
+//  Copyright (c) 2012 Hao Zhuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewControllerScore : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *scoreR;
+@property (weak, nonatomic) IBOutlet UILabel *score;
+@property (weak, nonatomic) IBOutlet UILabel *resultR;
+
+@end
