87748aca01c680b03daa2c7c7e1c51e0160de6bb
leotail <leotail@users.noreply.github.com>
2015-06-14 20:04:28 -0700

    Create AnimalCard.m

---
+++ AnimalCard.m
@@ -0,0 +1,21 @@
+//
+//  AnimalCard.m
+//  iConcentrate
+//
+//  Created by Patrick Wong on 2012-11-12.
+//  Copyright (c) 2012 Team Asia. All rights reserved.
+// 
+
+#import "AnimalCard.h"
+
+@implementation AnimalCard
+
+@synthesize name;
+@synthesize posFirst;
+@synthesize posSecond;
+@synthesize playable;
+@synthesize guessedPos;
+@synthesize arrayId;
+
+
+@end
